2e943d0c3a1dc4ea9858bb62e9323643bf3b2fe7
Aun-Ali-12 <aunali29331@gmail.com>
2026-07-08 00:11:35 +0500

    react hooks

--- react_functions/src/App.jsx
+++ react_functions/src/App.jsx
@@ -3,24 +3,40 @@ import { useState } from 'react'
 
 const App = () => {
 
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState({user: 'John', age: 30})
 
   function handleClick() {
-    console.log('Button clicked!')
-    setCount(count + 1)
+
+    const newUser = {...user}
+    newUser.user = "Ali"
+    setUser(newUser)
+
   }
 
-  function handleReset() {
-    console.log('Reset button clicked!')
-    setCount(0)
-  }
+
+
+  // const [num, setNum] = useState(0)
+
+  // function handleClick() {
+  //   console.log('Button clicked!')
+  //   setCount(count + 1)
+  // }
+
+  // function handleReset() {
+  //   console.log('Reset button clicked!')
+  //   setCount(0)
+  // }
 
 
   return (
     <div>
+    {/* <h1>The value of num is: {num}</h1>
+      <button onClick={() => setNum(num + 1)}>Click me {num}</button>
+      <button onClick={() => setNum(0)}>Reset</button> */}
+
+      <h1>Welcome, {user.user}! your age is {user.age}</h1>
       <button onClick={handleClick}>Click me</button>
-      <button onClick={handleReset}>Reset</button>
-      <p>Count: {count}</p>
+
     </div>
   )
 }
